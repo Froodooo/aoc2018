7821274e074fee47c2f086437f40fb229d245428
Yordi Verkroost <yordi.verkroost@bettyblocks.com>
2018-12-04 08:38:21 +0100

    Correctly sort input

--- lib/day4/common.ex
+++ lib/day4/common.ex
@@ -3,12 +3,12 @@ defmodule AoC.DayFour.Common do
     path
     |> File.stream!()
     |> Stream.map(&String.trim_trailing/1)
-    |> Stream.map(fn x ->
+    |> Enum.to_list()
+    |> Enum.sort()
+    |> Enum.map(fn x ->
       [datetime, log] = Regex.run(~r/\[(.*)\] (.*)/, x, capture: :all_but_first)
       {:ok, datetime} = Timex.parse(datetime, "{YYYY}-{M}-{D} {h24}:{m}")
-        %{datetime: datetime, log: log}
+      %{datetime: Timex.to_datetime(datetime), log: log}
     end)
-    |> Enum.to_list()
-    |> Enum.sort_by(fn x -> x.datetime end)
   end
 end
