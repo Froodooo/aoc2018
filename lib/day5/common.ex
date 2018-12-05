a70b2aa77b2bfca74c2e1206dfbe9a2cb285e6db
Froodooo <yordiverkroost@hotmail.com>
2018-12-05 08:04:54 +0100

    Add framework for day 5

---
+++ lib/day5/common.ex
@@ -0,0 +1,5 @@
+defmodule AoC.DayFive.Common do
+  def read_input(path) do
+    IO.puts path
+  end
+end
---
+++ lib/day5/partone.ex
@@ -0,0 +1,7 @@
+defmodule AoC.DayFive.PartOne do
+  alias AoC.DayFive.Common
+
+  def main() do
+    Common.read_input("Hello world!")
+  end
+end
